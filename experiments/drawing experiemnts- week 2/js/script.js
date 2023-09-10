5d0e9f2aba227b41cc693155d37b9ba5eb9a075c
Léa Fournier <lea.fournier@gmail.com>
2023-09-10 11:40:41 -0400

    rainbow cone

--- experiments/drawing experiemnts- week 2/js/script.js
+++ experiments/drawing experiemnts- week 2/js/script.js
@@ -22,13 +22,25 @@ function preload() {
 function setup() {
 createCanvas(500, 500);
 
-background(128, 0, 0);
+background(52, 235, 207);
+
+stroke(242, 165, 237);
+fill(255, 199, 216); 
 
 ellipseMode(CORNER);
+fill(237, 0, 0);
 ellipse(250, 250, 100, 100);
+
+fill(255, 132, 0);
 ellipse(250, 250, 80, 80);
+
+fill(246, 255, 0);
 ellipse(250, 250, 60, 60); 
+
+fill(64, 255, 0);
 ellipse(250, 250, 40, 40);
+
+fill(255, 0, 225);
 ellipse(250, 250, 20, 20); 
 }
 
