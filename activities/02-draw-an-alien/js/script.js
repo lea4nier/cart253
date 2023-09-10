97bb3bde0a57ce40a5d92fa134d3f0b18cf337cf
Léa Fournier <lea.fournier@gmail.com>
2023-09-10 14:22:43 -0400

    activity 02: drew an amazing alien

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -6,21 +6,38 @@
  * and this description to match your project!
  */
 
-"use strict";
-
-/**
- * Description of preload
-*/
-function preload() {
-
-}
 
 
 /**
- * Description of setup
+ * draw the background 
 */
 function setup() {
+createCanvas(640, 480); 
+background(255, 201, 214);
+noStroke(); 
 
+//draw the body
+fill(138, 132, 134);
+ellipse(320, 480, 300, 200); 
+
+//draw the head
+fill(125, 120, 122); 
+ellipse(320, 240, 250, 400);
+
+//draw the eyes
+fill(15, 15, 15);
+ellipse(250, 240, 80, 250);
+ellipse(390, 240, 80, 250);
+
+//draw the nostrils
+ellipse(300, 350, 10, 10);
+ellipse(340, 350, 10, 10);
+
+//draw the mouth
+stroke(255, 0, 0);
+strokeWeight(4); 
+rectMode(CENTER); 
+rect(320, 390, 100, 25);
 }
 
 
