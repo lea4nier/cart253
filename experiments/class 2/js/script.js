6f6c181a4bfbbd04c19066fead90f604322fae0d
Léa Fournier <lea.fournier@gmail.com>
2023-09-13 08:13:55 -0400

    experimenting in class with css

---
+++ experiments/class 2/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+createCanvas(500,500); 
+background(212, 249, 250);
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
