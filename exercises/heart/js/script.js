8fa0507c7a2b54bcaf19b30f6b0b7c7ccb9a821a
Léa Fournier <lea.fournier@gmail.com>
2023-09-17 23:23:44 -0400

    heart experiment

---
+++ exercises/heart/js/script.js
@@ -0,0 +1,64 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * This is a template. You must fill in the title, author, 
+ * and this description to match your project!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+createCanvas (500,500)
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+fill(255,0,0);
+stroke(0,0,0,0); 
+rect(250, 250, 10, 105);
+rect(245, 245, 5, 105);
+rect(260, 240, 5, 105);
+rect(265, 240, 5, 100);
+rect(265, 243, 5, 95);
+rect(265, 245, 5, 100);
+rect(270, 235, 5, 106);
+rect(275, 233, 5, 105);
+rect(280, 230, 5, 105);
+rect(285, 233, 5, 95);
+rect(290, 235, 5, 85);
+rect(295, 237, 5, 80);
+rect(300, 239, 5, 75);
+rect(305, 241, 5, 55);
+rect(310, 243, 5, 50);
+
+
+
+
+rect(235, 233, 5, 95);
+rect(230, 235, 5, 85);
+rect(225, 237, 5, 80);
+rect(220, 239, 5, 75);
+rect(215, 241, 5, 55);
+rect(210, 243, 5, 50);
+
+rect(255, 245, 5, 105);
+rect(240, 240, 5, 105);
+
+rect(235, 240, 5, 100);
+
+}
--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -1,32 +1,93 @@
-/**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
+/**************************************************
+Template p5 project
+Pippin Barr
 
-"use strict";
+Here is a description of this template p5 project.
+**************************************************/
 
-/**
- * Description of preload
-*/
-function preload() {
+let bg = {
+    r: 161,
+    g: 220,
+    b: 255
+  };
+  let circle1 = {
+    x: 0,
+    y: 400,
+    size: 100,
+    growthRate: 1,
+    speed: 1, 
+    alpha: 200
+  };
+  let circle2 = {
+    x: 500,
+    y: 250,
+    size: 75,
+    sizeRatio: 0.5,
+    speed: -1,
+    fill: 255,
+    alpha: 200
+  };
+  
+  let triangle1 = {
+    x1: 500,
+    y1: 275,
+    x2: 58,
+    y2: 20,
+    x3: 86,
+    y3: 75,
+    speed: 1,  
+    
+  }; 
 
-}
+  // setup()
+  //
+  // Description of setup() goes here.
+  function setup() {
+    createCanvas(500,500);
+    noStroke();
+  }
+  
+  // draw()
+  //
+  // Description of draw() goes here.
+  function draw() {
 
+    // Background
+    background(bg.r,bg.g,bg.b);
+    bg.r = map(circle1.size,100,width,0,255);
 
-/**
- * Description of setup
-*/
-function setup() {
+    ellipseMode(CENTER);
+    ellipse(mouseX,mouseY,10,10);
+    circle1.size = map(mouseY,height,0,100,400);
+    
+    triangle1.x1 = triangle1.x1 + triangle1.speed; 
+    triangle1.x2 = triangle1.x2 + triangle1.speed;
+    triangle1.x3 = triangle1.x3 + triangle1.speed;
+    triangle1.y1 = triangle1.y1 + triangle1.speed;
+    triangle1.y2 = triangle1.y2 + triangle1.speed;
+    triangle1.y3 = triangle1.y3 + triangle1.speed;
+   
+    // Left circle
+    circle1.x = circle1.x + circle1.speed;
+    circle1.x = constrain(circle1.x,0,width/2);
+   //change color
+    circle1.fill = color(random(255), random(255), random(255)); // Get a random fill for our circle!
+  fill(circle1.fill); // Apply our circle's random fill
+   
+  ellipse(circle1.x, circle1.y, circle1.size);
+    
+    circle1.size = constrain(circle1.size,0,width);
+    
+    ellipse(circle1.x,circle1.y,circle1.size);
+  
+    // Right circle
+    circle2.x = circle2.x + circle2.speed;
+    circle2.x = constrain(circle2.x,width/2,width);
+   //change color
+    circle2.fill = color(random(255), random(255), random(255)); // Get a random fill for our circle!
+  fill(circle2.fill); // Apply our circle's random fill
+  ellipse(circle2.x, circle2.y, circle2.size);
 
-}
-
-
-/**
- * Description of draw()
-*/
-function draw() {
-
-}
+    
+    ellipse(circle2.x,circle2.y,circle2.size);
+  }
