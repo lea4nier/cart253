54b71318bc5263dd70ba9538b948a8356a616ea8
Léa Fournier <lea.fournier@gmail.com>
2023-09-25 22:56:48 -0400

    pageant queens

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -7,78 +7,51 @@
  */
 
 "use strict";
-let covid19 = {
-  x: 0,
-  y: 250,
-  size: 100,
-  vx: 0,
-  vy: 0,
-  speed: 5,
-  fill: {
-    r: 255,
-    g: 0,
-    b: 0
-  }
-};
+let crown; 
+let queen1; 
+let queen2;
 
-let user = {
-  x: 250,
-  y: 250,
-  size: 100,
-  fill: 255
-};
+let posX = 0; 
+let posY = 0; 
+let size1 = 100; 
+let size2 = 200; 
 
-let numStatic = 1000;
+
+function preload () {
+  crown = loadImage ("assets/images/crown.png")
+  queen1 = loadImage("assets/images/queen1.png");
+  queen2 = loadImage("assets/images/queen2.png"); 
+}
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  //covid19.y = random(0, height);
-  //covid19.vx = covid19.speed;
-
-  //noCursor();
 }
 
-function preload () {
-    queen1 = loadImage("assets/images/queen1.PNG");
-}
 
 function draw() {
-    background(245, 2, 140);
-  image(queen1,0,0);
+  background(245, 2, 140);
 
-  // Display static
-  for (let i = 0; i < numStatic; i++) {
-    let x = random(0, width);
-    let y = random(0, height);
-    stroke(255);
-    point(x, y);
+  text("WHO WILL WIN MISS UNIVERSE??", 155, 100);
+
+  image(crown, mouseX, mouseY, 50, 50);
+
+  image(queen1,posX,posY, size1, size1+20);
+  queen1.resize(90,200);
+
+  image(queen2, 200, 400 - posY, size2, size2+20); 
+
+  if (posX > width || posY > height) {
+    posX = 0
+    posY = 0
+    size1 = 100
+    size2 = 200
+
+  } else {
+    posX += 0.9
+    posY += 0.9
+    size1 += 0.2
+    size2 -=0.25
   }
 
-  // Covid 19 movement
-  covid19.x = covid19.x + covid19.vx;
-  covid19.y = covid19.y + covid19.vy;
-
-  if (covid19.x > width) {
-    covid19.x = 0;
-    covid19.y = random(0, height);
-  }
-
-  // User movement
-  user.x = mouseX;
-  user.y = mouseY;
-
-  // Check for catching covid19
-  let d = dist(user.x, user.y, covid19.x, covid19.y);
-  if (d < covid19.size / 2 + user.size / 2) {
-    noLoop();
-  }
-
-  // Display covid 19
-  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
-  ellipse(covid19.x, covid19.y, covid19.size);
-
-  // Display user
-  fill(user.fill);
-  ellipse(user.x, user.y, user.size);
 }
