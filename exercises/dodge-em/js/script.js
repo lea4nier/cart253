81eb1a43fae126829991a5b734bb409d1fd8500b
Léa Fournier <lea.fournier@gmail.com>
2023-09-25 23:37:15 -0400

    final edits for the night

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -1,29 +1,28 @@
 /**
- * Title of Project
- * Author Name
+ * Dodge-Em Exercise
+ * Léa Fournier
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Miss Universe 2023
  */
 
-"use strict";
+
 let crown; 
 let queen1; 
 let queen2;
 
-let posX = 0; 
-let posY = 0; 
+let X = 0; 
+let Y = 0; 
 let size1 = 100; 
 let size2 = 200; 
 
-
+// loading images
 function preload () {
-  crown = loadImage ("assets/images/crown.png")
+  crown = loadImage ("assets/images/crown.png")     
   queen1 = loadImage("assets/images/queen1.png");
   queen2 = loadImage("assets/images/queen2.png"); 
 }
 
-
+// create canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -32,27 +31,35 @@ function setup() {
 function draw() {
   background(245, 2, 140);
 
+  //display text 
   textSize(20);
   text("WHO WILL WIN MISS UNIVERSE??", 155, 100);
   fill(255);
 
-  image(crown, mouseX, mouseY, 60, 60);
+  // display crown and pageants queens 
+  image(crown, mouseX, mouseY, 70, 70);
 
-  image(queen1,posX,posY, size1, size1+20);
+  image(queen1,X,Y, size1, size1+20);
 
-  image(queen2, 200, 400 - posY, size2, size2+20); 
+  image(queen2, 200, 400 - Y, size2, size2+20); 
 
-  if (posX > width || posY > height) {
-    posX = 0
-    posY = 0
-    size1 = 100
+
+  // pageant queens move 
+  if (X > width || Y > height) {
+    X = 0
+    Y = 0
+    size1 = 200
     size2 = 200
 
   } else {
-    posX += 3
-    posY += 3
-    size1 += 0.5
-    size2 -=0.25
+    X += 3
+    Y += 3
+    size1 += 0.25
+    size2 -= 0.25
   }
 
+}
+
+function mousePressed(){
+  
 }
