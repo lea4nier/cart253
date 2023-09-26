bc10874dd78ef2e4795d0015e5e62c3889d92d13
Léa Fournier <lea.fournier@gmail.com>
2023-09-25 23:12:39 -0400

    faster??

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -36,10 +36,9 @@ function draw() {
   text("WHO WILL WIN MISS UNIVERSE??", 155, 100);
   fill(255);
 
-  image(crown, mouseX, mouseY, 50, 50);
+  image(crown, mouseX, mouseY, 60, 60);
 
   image(queen1,posX,posY, size1, size1+20);
-  queen1.resize(90,200);
 
   image(queen2, 200, 400 - posY, size2, size2+20); 
 
@@ -50,9 +49,9 @@ function draw() {
     size2 = 200
 
   } else {
-    posX += 0.9
-    posY += 0.9
-    size1 += 0.2
+    posX += 3
+    posY += 3
+    size1 += 0.5
     size2 -=0.25
   }
 
