5fcbd59ba9cf57efc7499effa2a45ce3343d8426
Léa Fournier <lea.fournier@gmail.com>
2023-09-30 16:54:26 -0400

    changing how we call function

--- experiments/week 5/js/script.js
+++ experiments/week 5/js/script.js
@@ -6,54 +6,27 @@
  * and this description to match your project!
  */
 
-"use strict";
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  vx: 1,
-  vy: 0
-}
-
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
   background(0);
-
-  move(); // Call our move function so the circle position is updates
-  wrap(); // Call our wrap function so the circle moves back to the left if it reaches the right
-  display(); // Call our display function so the circle is displayed
+  parallels(100, 200);
+  parallels(200, 100);
+  parallels(200, 300);
 }
 
-// Defining our move function
-function move() {
-  circle.x = circle.x + circle.vx;
-  circle.x = circle.x + circle.vy;
-}
-
-// Defining our wrap function
-function wrap() {
-  if (circle.x > width) {
-    reset();
+// Defining the function
+function parallels(x,y) {
+  // We don't need to declare x and y now because they are provided as parameters
+  for (let i = 0; i < 20; i++) {
+    noStroke();
+    fill(255);
+    rectMode(CENTER);
+    // We can still use x and y as variables
+    rect(x, y, 2, 50);
+    // Including changing x inside our loop
+    x = x + 5;
   }
-}
-
-// Defining our reset function
-function reset() {
-  circle.x = 0;
-  circle.vx = circle.vx + 1;
-  circle.size = circle.size + 5;
-}
-
-// Defining our display function
-function display() {
-  fill(255,0,0);
-  ellipse(circle.x, circle.y, circle.size);
-}
-
-function mousePressed() {
-  // CALLING reset()
-  reset();
 }
