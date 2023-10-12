a6686a8331c23a203807ef8e21473832e28ff6b6
Léa Fournier <lea.fournier@gmail.com>
2023-10-12 12:08:14 -0400

    first two eyes can be dragged!

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -12,13 +12,46 @@ let cat2;
 let banner;
 let banner2; 
 let instructions1;
+let pumpkin;
 
-let pumpkin; 
+let eyea;
+let eyeb; 
 
-let state = "title"
+
+// to check if any obj is being dragged
+let objisbeingdragged =false;
+
+let eyea_obj = {
+    x:0,
+    y:0,
+    w:100,
+    h:50
+};
+
+let eyeb_obj = {
+    x: 0,
+    y: 100, 
+    w: 100, 
+    h: 50
+}
+
+let objBeingDragged = {
+    x:0,
+    y:0,
+    w:0,
+    h:0
+}
+
+var dragging = false; // Is the object being dragged?
+var rollover = false; // Is the mouse over the ellipse?
+
+var x, y, w, h; // Location and size
+var offsetX, offsetY; // Mouseclick offset
+
+let state = "title"; 
 
 /**
- * Description of preload
+ preload images 
 */
 function preload() {
     cat2 = loadImage('assets/images/cat2.png');
@@ -26,14 +59,23 @@ function preload() {
     banner2 = loadImage('assets/images/banner2.png');
     instructions1 = loadImage('assets/images/instructions.png');
     pumpkin = loadImage('assets/images/pumpkin.png');
+    eyea = loadImage('assets/images/eyea.png');
+    eyeb = loadImage('assets/images/eyeb.png');
 }
 
 
 /**
- * Description of setup
+ * Description 
 */
 function setup() {
 createCanvas(500,500); 
+
+
+// x = 0;
+// y = 0; 
+
+// w = 100;
+// h = 50;
 }
 
 
@@ -72,10 +114,66 @@ function instructions(){
 }
 
 function mousePressed() {
-    if (state === `instructions`) 
+    if (state === `instructions`) {
         state = `game`;
+    }
+if(state ===`game`){
+    if(objisbeingdragged ===false){
+    if (mouseX > eyea_obj.x && mouseX < eyea_obj.x + eyea_obj.w && mouseY > eyea_obj.y && mouseY < eyea_obj.y + eyea_obj.h) {
+        dragging = true;
+        objisbeingdragged =true;
+        console.log("over a")
+        // assign the properties of eye_a to objBeingDragged
+        objBeingDragged = eyea_obj;
+
+
+        console.log(objBeingDragged)
+      } else if(mouseX > eyeb_obj.x && mouseX < eyeb_obj.x + eyeb_obj.w && mouseY > eyeb_obj.y && mouseY < eyeb_obj.y + eyeb_obj.h) {
+         dragging = true;
+         objisbeingdragged =true;
+         console.log("over b")
+          // assign the properties of eye_b to objBeingDragged
+         objBeingDragged = eyeb_obj;
+         console.log(objBeingDragged)
+      }
+    }
+}
+           
 }
 
 function game(){
     image(pumpkin, 60, 80, 400, 250);
+    
+    if(objisbeingdragged===true){
+    if (mouseX > objBeingDragged.x && mouseX < objBeingDragged.x + objBeingDragged.w && mouseY > objBeingDragged.y && mouseY < objBeingDragged.y + objBeingDragged.h) {
+       rollover = true;
+       console.log("over a")
+    }
+
+     else {
+        rollover = false;
+        console.log("no rollover")
+     }
+    }
+
+    
+      if (dragging) {
+       objBeingDragged.x = mouseX-objBeingDragged.w/2;
+       objBeingDragged.y = mouseY-objBeingDragged.h/2;
+
+        }
+    
+    stroke(0);
+    image(eyea, eyea_obj.x, eyea_obj.y, eyea_obj.w, eyea_obj.h);
+    image(eyeb, eyeb_obj.x, eyeb_obj.y, eyeb_obj.w, eyeb_obj.h); 
+ 
+
+        
+
 }
+
+function mouseReleased() {
+    // Quit dragging
+    dragging = false;
+    objisbeingdragged =false;
+  }
