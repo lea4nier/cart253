6306a956f48005c2b1e9267cb63a90796cc0cfc7
Léa Fournier <lea.fournier@gmail.com>
2023-10-23 20:41:01 -0400

    fish can be moved by user

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -7,6 +7,13 @@
  */
 
 "use strict";
+let circle = {
+    x: 0,
+    y: 0,
+    size: 50,
+    trail: [] // Note that we are creating an EMPTY TRAIL ARRAY as a PROPERTY of the circle
+  }
+
 let fish;
 let shark;
 let home;
@@ -35,7 +42,7 @@ createCanvas(600,600);
  * Description of draw()
 */
 function draw() {
-    background(2, 170, 247);
+    background(99, 182, 255);
     if (state === "title") {
         title();
     }
@@ -51,4 +58,21 @@ function title(){
 function keyPressed() {
     if (keyCode === 32)
     state = 'game'; 
+}
+
+function game(){
+    for (let i = 0; i < circle.trail.length; i++) {
+        // Get the element at the index indicated by i (0, then 1, then 2, etc.)
+        let element = circle.trail[i];
+        // Draw an ellipse the same size as the circle at that position
+        ellipse(element.x, element.y, circle.size);
+      }
+    
+      // Move the circle to the mouse position
+      circle.x = mouseX;
+      circle.y = mouseY;
+    
+      // Draw the circle
+      ellipse(circle.x, circle.y, circle.size);
+
 }
