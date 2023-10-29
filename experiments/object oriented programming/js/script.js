24fc96c7ee3db4e1eb0fca87ff4f8c68362a99a1
Léa Fournier <lea.fournier@gmail.com>
2023-10-29 19:52:03 -0400

    added bees!

--- experiments/object oriented programming/js/script.js
+++ experiments/object oriented programming/js/script.js
@@ -8,12 +8,15 @@
 
 "use strict";
 // Our garden
-// Our garden
 let garden = {
     // An array to store the individual flowers
     flowers: [],
     // How many flowers in the garden
     numFlowers: 20,
+    // An array to our the bees
+    bees: [],
+    // How many bees in the garden
+    numBees: 5,
     // The color of the grass (background)
     grassColor: {
       r: 120,
@@ -43,6 +46,18 @@ let garden = {
       // Add the flower to the array of flowers
       garden.flowers.push(flower);
     }
+  
+    // Create our bees by counting up to the number of bees
+    for (let i = 0; i < garden.numBees; i++) {
+      // Create variables for our arguments for clarity
+      let x = random(0, width);
+      let y = random(0, height);
+      // Create a new bee using the arguments
+      let bee = new Bee(x, y);
+      // Add the bee to the array of bees
+      garden.bees.push(bee);
+    }
+  
   }
   
   
@@ -55,11 +70,23 @@ let garden = {
     // Loop through all the flowers in the array and display them
     for (let i = 0; i < garden.flowers.length; i++) {
       let flower = garden.flowers[i];
-      // NEW! Check if this flower is alive before updating it
+      // Check if this flower is alive
       if (flower.alive) {
         // Update the flower by shrinking it and displaying it
-        flower.shrink(); // NEW! Shrink living flowers every frame
+        flower.shrink();
         flower.display();
+      }
+    }
+  
+    // Loop through all the bees in the array and display them
+    for (let i = 0; i < garden.bees.length; i++) {
+      let bee = garden.bees[i];
+      // Check if this bee is alive
+      if (bee.alive) {
+        // Update the bee by shrinking, moving and displaying it
+        bee.shrink();
+        bee.move();
+        bee.display();
       }
     }
   }
