66c663b08d51a542cfa77433e007b044d12b83e5
Léa Fournier <lea.fournier@gmail.com>
2023-10-30 20:37:19 -0400

    display paddle

--- activities/object-oriented-programming-activity/js/Paddle.js
+++ activities/object-oriented-programming-activity/js/Paddle.js
@@ -10,4 +10,13 @@ class Paddle {
     move(){
         this.x = mouseX;
     }
+
+    display(){
+        push();
+        fill(255);
+        noStroke();
+        rectMode(CENTER);
+        rect(this.x,this.y,this.width, this.height);
+        pop();
+    }
 }
