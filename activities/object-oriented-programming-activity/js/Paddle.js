0b2e4891f086d9a55de5f0e635fc6272d7c8529d
Léa Fournier <lea.fournier@gmail.com>
2023-10-30 20:33:37 -0400

    add constructor + move method

--- activities/object-oriented-programming-activity/js/Paddle.js
+++ activities/object-oriented-programming-activity/js/Paddle.js
@@ -1,3 +1,13 @@
 class Paddle {
-    
+
+    constructor(w,h){
+       this.width = w; 
+       this.height = h; 
+       this.x = 0;
+       this.y = height - this.height/2; 
+    }
+
+    move(){
+        this.x = mouseX;
+    }
 }
