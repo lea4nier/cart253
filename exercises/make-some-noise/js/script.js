d573709a42a42d647a597d1d8d3cb04fe2d090ce
Léa Fournier <lea.fournier@gmail.com>
2023-11-14 11:31:32 -0500

    bottle animates

--- exercises/make-some-noise/js/script.js
+++ exercises/make-some-noise/js/script.js
@@ -7,28 +7,28 @@
  */
 
 "use strict";
+let frames = [];
+let currentFrame = 0;
 
-let drink = [];
-
-let animate = 0;
-
-let time = 0;
-
-let fr = 10;
+let fr = 1;
 
 /**
  * Description of preload
 */
 function preload() {
-    drink[0] = loadImage('assets/images/bottle1.png');
-    drink[1] = loadImage('assets/images/bottle2.png');
-    drink[2] = loadImage('assets/images/bottle3.png');
-    drink[3] = loadImage('assets/images/bottle4.png');
-    drink[4] = loadImage('assets/images/bottle5.png');
-    drink[5] = loadImage('assets/images/bottle6.png');
-    drink[6] = loadImage('assets/images/bottle7.png');
-    drink[7] = loadImage('assets/images/bottle8.png');
-    drink[8] = loadImage('assets/images/bottle9.png');
+    for (let i = 1; i <= 9; i++) {
+        let frame = loadImage('assets/images/bottle' + i + '.png');
+        frames.push(frame);
+    }
+    // drink[0] = loadImage('assets/images/bottle1.png');
+    // drink[1] = loadImage('assets/images/bottle2.png');
+    // drink[2] = loadImage('assets/images/bottle3.png');
+    // drink[3] = loadImage('assets/images/bottle4.png');
+    // drink[4] = loadImage('assets/images/bottle5.png');
+    // drink[5] = loadImage('assets/images/bottle6.png');
+    // drink[6] = loadImage('assets/images/bottle7.png');
+    // drink[7] = loadImage('assets/images/bottle8.png');
+    // drink[8] = loadImage('assets/images/bottle9.png');
 }
 
 
@@ -37,8 +37,6 @@ function preload() {
 */
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(0, 0, 0);
-    frameRate(fr);
 }
 
 
@@ -46,16 +44,9 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    fill(0);
+    background(0, 0, 0);
     frameRate(fr);
-    image(drink[animate], windowWidth / 3, windowHeight / 4, 400, 400);
-    if (time < 10) {
-
-        // move to the next index in the array
-        animate = animate + 1;
-        // reset frame timer
-        // time = 0;
-    }
-
-    time++;
+    image(frames[currentFrame], windowWidth / 3, windowHeight / 4, 400, 400);
+    // Advance to the next frame (and loop back to 0 if you reach the end)
+    currentFrame = (currentFrame + 1);
 }
