0aed6a6b9c9e45dbdc432c0022371687514ae5e7
Léa Fournier <lea.fournier@gmail.com>
2023-11-25 13:58:18 -0500

    strobe effect when cat appears

--- projects/project2/cat/js/script.js
+++ projects/project2/cat/js/script.js
@@ -11,9 +11,11 @@ let catimage;
 let choice1;
 let choice2;
 let choice3;
-let catSize = 50; // Initial size
+let size = 50; // Initial size
 let growthRate = 0.5; // Rate of size increases
-let state = "appear"
+let strobeCat = "true";
+
+let state = "appear";
 /**
  * Description of preload
  */
@@ -47,28 +49,41 @@ function draw() {
 function hello() {
     background(0, 0, 0);
 
-    // Increase the cat size
-    catSize += growthRate;
+    // Increases the cat size
+    size += growthRate;
 
-    // Check if the size exceeds the limit
-    if (catSize > 500) {
-        catSize = 500; // Set size to the limit
+    // Check if the size of the cat exceeds the limit
+    if (size > 500) {
+        size = 500; // Set size to the limit
     }
 
-    if (catSize === 500) {
+    if (strobeCat) {
+        // cat appears at growing size 
+        image(catimage, windowWidth / 3, windowHeight / 4, size, size);
+    }
+
+    strobeCat = !strobeCat; //image of cat dissapears and reappears to give strobe effect
+
+    if (size === 500) {  //once cat is 500px the state switches to talking
         state = "talking";
     }
 
-    // Draw the cat image with the updated size
-    image(catimage, windowWidth / 3, windowHeight / 4, catSize, catSize);
+
 }
 
 function talkingcat() {
     background(0, 0, 0);
     image(catimage, windowWidth / 3, windowHeight / 4, 500, 500);
-    image(choice1, (windowWidth / 3) + 400, windowHeight / 4, 400, 400);
-    image(choice2, (windowWidth / 3) + 400, (windowHeight / 4) + 300, 400, 400);
-    image(choice3, (windowWidth / 3) - 350, windowHeight / 4, 400, 400);
+    // Increases the text bubble sizes
+    size += growthRate;
+
+    if (size > 400) {
+        size = 400; // Set size to the limit
+    }
+
+    image(choice1, (windowWidth / 3) + 400, windowHeight / 4, size, size);
+    image(choice2, (windowWidth / 3) + 400, (windowHeight / 4) + 300, size, size);
+    image(choice3, (windowWidth / 3) - 350, windowHeight / 4, size, size);
 
 
 }
