68785606f6b3252782fe0d92e6453b2bdff146a6
Léa Fournier <lea.fournier@gmail.com>
2023-11-25 10:43:58 -0500

    started cheshire cat game

--- projects/project2/project2/js/script.js
+++ projects/project2/project2/js/script.js
@@ -1,33 +1,14 @@
-/**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
-
-"use strict";
-let clickCount = 0;
-let typewriter;
-
-let aliceAsset;
-let gravityForce = 0.0025;
-
-let alices = [];   //create an array for the apples
-let numAlices = 1;
-
-let cupAsset;
-let cupImages = [];
-let cups = [];
-let numCups = 6;
+// A variable to store the currently active state object (starts empty)
+let currentState;
 
 let hole;
+let aliceAsset;
+let cupImages = [];
 
-let state = "title";
 
-/**
- * Description of preload
-*/
+// setup()
+// Create the canvas, start our program in the title state, set default text style
+
 function preload() {
     hole = loadImage('assets/images/down.png');
     aliceAsset = loadImage('assets/images/fallAl.png');
@@ -37,108 +18,28 @@ function preload() {
     }
 }
 
-
-/**
- * Description of setup
-*/
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    typewriter = new Typewriter();
 
-    for (let i = 0; i < numAlices; i++) {
-        let x = windowWidth / 2;      //good apples fall at a random x and y
-        let y = -400;
-        let alice = new Alice(x, y, aliceAsset);
-        alices.push(alice);
-    }
+    // We can set the current state by creating a NEW object from the class
+    // representing that state! This will call its constructor() which will work
+    // like the `setup()` for that state.
+    currentState = new Title1();
 
-    for (let i = 0; i < cupImages.length; i++) {
-        let x = random(windowWidth / 2, windowWidth - windowWidth / 2.7);
-        let y = random(windowHeight + 100, windowHeight + 1000);
-        let cupObject = new Cup(x, y, cupImages[i]);
-        cups.push(cupObject);
-    }
 
 }
 
-
-/**
- * Description of draw()
-*/
+// draw()
+// Simply call the draw method of the current state
 function draw() {
-    if (state === "title") {
-        game();
-    }
-    else if (state === "one") {
-        one();
-    }
-    else if (state === "two") {
-        two();
-    }
+    // If the current state is Title this will call the Title class draw()
+    // If the current state is Animation this will call the Animation class draw()
+    // if the current state is Ending this will call the Ending class draw()
+    currentState.draw();
 }
 
 
-
-function game() {
-    background(0, 0, 0);
-    image(hole, windowWidth / 18, windowHeight / 120, windowWidth, windowHeight);
-    typewriter.display();
-
-    for (let i = 0; i < alices.length; i++) {
-
-        if (alices[i].active) { //calls the functions in the apple class that drops the apples
-            alices[i].gravity(gravityForce);
-            alices[i].move();
-            alices[i].display();
-        }
-
-    }
-}
 
 function mousePressed() {
-    clickCount++;
-    if (clickCount === 1) {
-        typewriter.typewrite(`Alice in Wonderland`, windowWidth / 18, windowHeight / 2);
-    }
-
-    else if (clickCount === 2) {
-        state = "one";
-        typewriter.typewrite(`click to continue`, windowWidth / 18, windowHeight / 2);
-    }
-
-    else if (clickCount == 3) {
-        state = "two";
-    }
+    currentState.mousePressed();
 }
-
-function one() {
-    background(0, 0, 0);
-    image(hole, windowWidth / 18, windowHeight / 120, windowWidth, windowHeight);
-    typewriter.display();
-    for (let i = 0; i < alices.length; i++) {
-
-        if (alices[i].active) { //calls the functions in the apple class that drops the apples
-            alices[i].follow();
-            alices[i].display();
-        }
-
-    }
-
-    for (let i = 0; i < cups.length; i++) {
-        cups[i].display();
-        cups[i].gravity();
-        cups[i].move();
-    }
-}
-
-function two() {
-    background(0, 0, 0);
-    for (let i = 0; i < alices.length; i++) {
-        if (alices[i].active) { //calls the functions in the apple class that drops the apples
-            alices[i].fall();
-            alices[i].gravity(gravityForce);
-            alices[i].display();
-        }
-    }
-}
-
---
+++ projects/project2/project2/js/title1.js
@@ -0,0 +1,38 @@
+class Title1 {
+
+    // constructor()
+    // Acts as the setup() of the state, called when the
+    // state is created. Sets the title of the program.
+    constructor() {
+
+        this.typewriter = new Typewriter();
+        this.gravityForce = 0.0025;
+        this.alices = [];   //create an array for the apples
+        this.numAlices = 1;
+        this.typewriter.typewrite(`Alice in Wonderland`, windowWidth / 18, windowHeight / 2);
+    }
+
+    // draw()
+    // Called every frame in the main script. Handles what the title
+    // state needs to do each frame, which is display the title.
+    draw() {
+        background(0, 0, 0);
+        image(hole, windowWidth / 18, windowHeight / 120, windowWidth, windowHeight);
+        this.typewriter.display();
+
+        for (let i = 0; i < this.alices.length; i++) {
+
+            if (this.alices[i].active) { //calls the functions in the apple class that drops the apples
+                this.alices[i].gravity(this.gravityForce);
+                this.alices[i].move();
+                this.alices[i].display();
+            }
+
+        }
+    }
+
+
+    mousePressed() {
+        currentState = new Title2();
+    }
+}
